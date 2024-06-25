0510e83028c71f8c18a12e58479ad7fb4c0c6f2b
Saikat Samanta <95569573+psykat1116@users.noreply.github.com>
2024-06-25 18:05:10 +0530

    Add Opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ const myFont2 = localFont({
 export const metadata: Metadata = {
   title: "Saikat Samanta | Full Stack Developer and UI/UX Designer",
   description:
-    "Welcome to my website I am Saikat Samanta, a passionate and innovative full stack developer & UI/UX designer. With a keen eye for detail and a dedication to crafting seamless user experiences, I specializes in building responsive, user-friendly websites and applications. Explore a curated selection of projects showcasing proficiency in front-end and back-end development, leveraging the latest technologies to deliver cutting-edge solutions. Dive into the About section to learn more about my journey, skills, and professional experiences. Connect through the Contact page for collaboration opportunities or to discuss potential projects. Let's build something amazing together!",
+    "Hello folks, Welcome to my personal portfolio website. I am Saikat Samanta, a passionate and innovative full stack developer & UI/UX designer. With a keen eye for detail and a dedication to crafting seamless user experiences, I specializes in building responsive, user-friendly websites and applications. Explore a curated selection of projects showcasing proficiency in front-end and back-end development, leveraging the latest technologies to deliver cutting-edge solutions. Dive into the About section to learn more about my journey, skills, and professional experiences. Connect through the Contact page for collaboration opportunities or to discuss potential projects. Let's build something amazing together!",
   icons: [
     {
       media: "(prefers-color-scheme: dark)",
@@ -35,6 +35,24 @@ export const metadata: Metadata = {
       href: "/LogoDark.svg",
     },
   ],
+  openGraph: {
+    title: "Saikat Samanta | Full Stack Developer and UI/UX Designer",
+    description:
+      "Hello folks, Welcome to my personal portfolio website. I am Saikat Samanta, a passionate and innovative full stack developer & UI/UX designer. With a keen eye for detail and a dedication to crafting seamless user experiences, I specializes in building responsive, user-friendly websites and applications.",
+    locale: "en-IN",
+    siteName: "Saikat Samanta's Portfolio",
+    url: "https://portfolio-one-gilt-34.vercel.app/",
+    countryName: "India",
+    images: [
+      {
+        url: "/profile.jpg",
+        alt: "Opengraph Image",
+        secureUrl: "/profile.jpg",
+        type: "image/jpg",
+      },
+    ],
+  },
+  metadataBase: new URL("https://portfolio-one-gilt-34.vercel.app/"),
 };
 
 export default function RootLayout({
